061afc80cb56d28a41d4e39247db4e309403dc44
ykrantz <ykrantz@gmail.com>
2022-05-13 18:30:58 +0300

    change select serch results to list

--- src/components/CitiesSearchResults/CitiesSearchResults.jsx
+++ src/components/CitiesSearchResults/CitiesSearchResults.jsx
@@ -23,33 +23,46 @@ const CitiesSearchResults = () => {
     <div>
       {/* <h5>serach results: </h5> */}
       {searchCitiesResults.length > 0 && (
-        <select
-          className="form-select  w-75 CitiesSearchResults-selectResults"
-          size={
-            searchCitiesResults.length >= 3 ? "3" : searchCitiesResults.length
-          }
-          onChange={(e) => {
-            handlePickedCityInFromSearchResults(e.target.value);
-          }}
+        // <select
+        //   className="form-select  w-75 CitiesSearchResults-selectResults"
+        //   size={
+        //     searchCitiesResults.length >= 3 ? "3" : searchCitiesResults.length
+        //   }
+        //   onChange={(e) => {
+        //     handlePickedCityInFromSearchResults(e.target.value);
+        //   }}
+        // >
+        //   {searchCitiesResults.map((val) => (
+        //     <option key={val} value={val}>
+        //       {val}
+        //     </option>
+        //   ))}
+        // </select>
+
+        <ul
+          className="list-group  w-75 CitiesSearchResults-selectResults"
+          // size={
+          //   searchCitiesResults.length >= 3 ? "3" : searchCitiesResults.length
+          // }
+          // onClick={(e) => {
+          //   handlePickedCityInFromSearchResults(e.target.value);
+          // }}
         >
           {searchCitiesResults.map((val) => (
-            <option key={val} value={val}>
+            <li
+              class="list-group-item"
+              key={val}
+              onClick={(e) => {
+                console.log(e.target.innerHTML);
+                handlePickedCityInFromSearchResults(e.target.innerHTML);
+              }}
+            >
               {val}
-            </option>
+            </li>
           ))}
-        </select>
+        </ul>
       )}
       {spinnerWaitingForData && <SniperWaitingCircle />}
-
-      {/* <select className="test" size="4">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>3</option>
-        <option>3</option>
-        <option>3</option>
-        <option>3</option>
-      </select> */}
     </div>
   );
 };
